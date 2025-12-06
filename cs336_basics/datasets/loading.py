991b190818f1d5aca5e4a31885422bca4e62fec8
shihaofu2003 <fshihao900@gmail>
2025-12-07 00:03:54 +0800

    load valid dataset

--- cs336_basics/datasets/loading.py
+++ cs336_basics/datasets/loading.py
@@ -23,6 +23,40 @@ def data_loading(dataset: npt.NDArray,
     return batch.to(device=device), target.to(device=device)
 
 
+def valid_data_loading(dataset: npt.NDArray,
+                       batch_size: int,
+                       context_length: int,
+                       device: torch.device) -> tuple[torch.Tensor, torch.Tensor]:
+    """
+    这是一个生成器，用于在验证阶段按顺序遍历整个数据集
+    """
+    data = torch.from_numpy(dataset).long()
+    n_samples = data.shape[0]
+
+    step = context_length
+    indices = torch.arange(0, n_samples - context_length, step)
+
+    for i in range(0, len(indices), batch_size):
+        batch_indices = indices[i: i + batch_size]
+
+        current_batch_size = len(batch_indices)
+        if current_batch_size == 0:
+            break
+
+        x_list = []
+        y_list = []
+
+        for start_idx in batch_indices:
+            end_idx = start_idx + context_length
+            x_list.append(data[start_idx: end_idx])
+            y_list.append(data[start_idx + 1: end_idx + 1])
+
+        batch = torch.stack(x_list)
+        target = torch.stack(y_list)
+
+        yield batch.to(device=device), target.to(device=device)
+
+
 class MemmapDataset(torch.utils.data.Dataset):
     def __init__(self, path, shape):
         self.data = np.memmap(path, dtype=np.int32, mode='r', shape=shape)
